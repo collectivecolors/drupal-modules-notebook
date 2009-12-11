81af7a7e8f335b7e5edfe4a4a881a4c12eb7fda2
Derek Webb <derek@collectivecolors.com>
2009-12-11 17:42:30 -0500

    Added some functionality to notebook.js.

--- notebook.js
+++ notebook.js
@@ -1,6 +1,6 @@
 // $Id$ 
 Drupal.behaviors.noteBookInitialize = function(context) {
-	// Some jQuery goes here
-	
-	
+	var $body = $(body);
+	var link = '<a href="ajaxOverFlow.html?height=300&width=300" title="Create a note for later reference." class="thickbox add-note"><img src="/sites/all/modules/notebook/note.jpg"></a>';  
+	$body.appendTo(link);
 }
